b8e252b434c89a15f6c6d85f746dc6d9fd4e6471
叶格辉 <ygh@aiitec.org>
2020-04-23 16:39:44 +0800

    test

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,59 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view>
+      <!-- <text class="title">{{ title }}</text> -->
+    </view>
+    <view>
+      <text class="title" @click="goto('page-1')">page-1</text>
+    </view>
+    <view>
+      <text class="title" @click="goto('page-2')">page-2</text>
+    </view>
+    <view>
+      <text class="title" @click="goto('page-3')">page-3</text>
+    </view>
+    <view>
+      <text class="title" @click="goto('page-4')">page-4</text>
+    </view>
+    <view v-if="!show">没有执行 onLoad</view>
+    <view v-if="show"> onLoad 已执行</view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  data() {
+    return {
+      title: "Hello",
+      show: false,
+    };
+  },
+  onLoad() {
+    this.show = true;
+  },
+  methods: {
+    goto(name) {
+      this.$Router.push({ name });
+    },
+  },
+};
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
+.content {
+  text-align: center;
+  height: 400upx;
+}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
+.logo {
+  height: 200upx;
+  width: 200upx;
+  margin-top: 200upx;
+}
 
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+.title {
+  font-size: 36upx;
+  /* color: #8f8f94; */
+}
 </style>
--- src/pages/tab-5/tab-5.vue
+++ src/pages/tab-5/tab-5.vue
@@ -1,9 +1,29 @@
 <template>
-  <view>tab-5</view>
+  <view>
+    tab-5
+    <view>
+      <text class="title" @click="goto('page-1')">page-1</text>
+    </view>
+    <view>
+      <text class="title" @click="goto('page-2')">page-2</text>
+    </view>
+    <view>
+      <text class="title" @click="goto('page-3')">page-3</text>
+    </view>
+    <view>
+      <text class="title" @click="goto('page-4')">page-4</text>
+    </view>
+  </view>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    goto(name) {
+      this.$Router.push({ name });
+    },
+  },
+};
 </script>
 
 <style></style>
